eb52e0b28b7b3a308485eace560ee232dc43c13b
Shane Argo <shane@shaneargo.net>
2014-05-19 22:09:05 +1000

    Initial commit

---
+++ src/main/java/com/alltheducks/bb/stripes/BlackboardActionBeanContext.java
@@ -0,0 +1,36 @@
+package com.alltheducks.bb.stripes;
+
+import blackboard.data.course.Course;
+import blackboard.data.user.User;
+import blackboard.persist.Id;
+import blackboard.platform.context.Context;
+import blackboard.platform.context.ContextManagerFactory;
+import net.sourceforge.stripes.action.ActionBeanContext;
+
+public class BlackboardActionBeanContext extends ActionBeanContext {
+    
+    Context context;
+    
+    public Context getBlackboardContext() {
+        if (context == null) {
+            context = ContextManagerFactory.getInstance().getContext();
+        }
+        return context;
+    }
+    
+    public Id getCourseId() {
+        return getBlackboardContext().getCourseId();
+    }
+    
+    public Course getCourse() {
+        return getBlackboardContext().getCourse();
+    }
+    
+    public Id getUserId() {
+        return getBlackboardContext().getUserId();
+    }
+    
+    public User getUser() {
+        return getBlackboardContext().getUser();
+    }
+}

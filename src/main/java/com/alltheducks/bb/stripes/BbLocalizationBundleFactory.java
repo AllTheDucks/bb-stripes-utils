546af85759daf513859b1af7853dfb307f77a7e6
Wiley <wiley.fuller@gmail.com>
2014-11-21 12:28:22 +1100

    Simplified implementation of Bb Localization Bundle Factory

--- src/main/java/com/alltheducks/bb/stripes/BbLocalizationBundleFactory.java
+++ src/main/java/com/alltheducks/bb/stripes/BbLocalizationBundleFactory.java
@@ -5,52 +5,47 @@ import blackboard.persist.Id;
 import blackboard.platform.intl.BbResourceBundle;
 import blackboard.platform.intl.BundleManager;
 import blackboard.platform.intl.BundleManagerFactory;
-import blackboard.platform.plugin.PlugIn;
 import blackboard.platform.plugin.PlugInManager;
 import blackboard.platform.plugin.PlugInManagerFactory;
-import java.lang.Exception;import java.lang.Override;import java.lang.String;import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 import net.sourceforge.stripes.config.Configuration;
 import net.sourceforge.stripes.localization.LocalizationBundleFactory;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 /**
  * Created by wiley on 20/11/14.
  */
 public class BbLocalizationBundleFactory implements LocalizationBundleFactory {
 
-    private PlugIn plugin;
-    private String vendorId;
-    private String handle;
+    public static final String VENDOR_INIT_PARAM = "blackboard.plugin.vendor";
+    public static final String PLUGIN_HANDLE_INIT_PARAM = "blackboard.plugin.handle";
+
+    private Id pluginId;
 
 
     @Override
     public ResourceBundle getErrorMessageBundle(Locale locale) throws MissingResourceException {
         BundleManager bm = BundleManagerFactory.getInstance();
-        BbResourceBundle bundle = bm.getPluginBundle(getPlugin(vendorId, handle).getId());
+        BbResourceBundle bundle = bm.getPluginBundle(pluginId);
         return bundle.getResourceBundle();
     }
 
     @Override
     public ResourceBundle getFormFieldBundle(Locale locale) throws MissingResourceException {
         BundleManager bm = BundleManagerFactory.getInstance();
-        Id pluginId = getPlugin(vendorId, handle).getId();
         BbResourceBundle bundle = bm.getPluginBundle(pluginId);
         return bundle.getResourceBundle();
     }
 
     @Override
     public void init(Configuration configuration) throws Exception {
-        vendorId = configuration.getBootstrapPropertyResolver().getProperty("blackboard.plugin.vendor");
-        handle = configuration.getBootstrapPropertyResolver().getProperty("blackboard.plugin.handle");
-    }
+        String vendorId = configuration.getBootstrapPropertyResolver().getProperty(VENDOR_INIT_PARAM);
+        String handle = configuration.getBootstrapPropertyResolver().getProperty(PLUGIN_HANDLE_INIT_PARAM);
 
-    private PlugIn getPlugin(String vendorId, String handle) {
-        if (plugin == null) {
-            PlugInManager pluginMgr = PlugInManagerFactory.getInstance();
-            plugin = pluginMgr.getPlugIn(vendorId, handle);
-        }
-        return plugin;
+        PlugInManager pluginMgr = PlugInManagerFactory.getInstance();
+        pluginId = pluginMgr.getPlugIn(vendorId, handle).getId();
     }
 
 }
